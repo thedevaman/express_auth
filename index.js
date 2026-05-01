acdbe1eb0a2c9ed7f04c684ae067255c902537c8
thedevaman <devaman2508@gmail.com>
2026-05-01 08:42:26 +0530

    post & get data on file

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const app = express()
+
+
+
 app.listen(8080,()=>console.log("Server is running"))
+app.use(express.json())
 app.get("/",(req,res)=>{
     const query = req.query
     console.log(query.name)
   res.send("success")
+})
+
+app.post("/",(req,res)=>{
+    const data = req.body
+    console.log(data)
+    res.send("success")
 })
